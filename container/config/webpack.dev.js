51d5332cb8fb742778626bb9489024f0eef389ae
kueben96 <kuebra.tokuc@haw-hamburg.de>
2023-02-14 17:28:20 +0100

    add production webpack config

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json')
@@ -15,9 +15,6 @@ const devConfig = {
         }
     },
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html'
-        }),
         new ModuleFederationPlugin({
             name: 'container',
             remotes: {
